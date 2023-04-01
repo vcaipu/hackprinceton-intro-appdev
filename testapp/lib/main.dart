54c03dd7b3ecfb5672712bebf4f163f48f381d2d
vcaipu <vc9839@princeton.edu>
2023-04-01 14:01:17 -0400

    commit

---
+++ testapp/lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fibonacci Number Generator',
+      theme: ThemeData( 
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Fibonacci Number Generator'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter1 = 1;
+  int _counter2 = 1;
+  int _count = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      int temp = _counter2;
+      _counter2 = _counter1 + _counter2;
+      _counter1 = temp;
+      _count++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Fibonacci Number:',
+            ),
+            Text(
+              '$_counter2',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), 
+    );
+  }
+}
